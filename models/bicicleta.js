c50887b7b16bb7d82ddbbeb9af9d713936f82d9a
diazag <adiazgp@gmail.com>
2023-02-07 09:22:30 -0500

    Commit_03

---
+++ models/bicicleta.js
@@ -0,0 +1,45 @@
+var Bicicleta = function (id,color,modelo,ubicacion){
+    this.id = id ;
+    this.color = color ;
+    this.modelo = modelo ;
+    this.ubicacion = ubicacion ;
+}
+
+Bicicleta.prototype.toString = function(){
+    return 'id: '+this.id+ " | color: "+ this.color ;
+}
+
+Bicicleta.allBicis = [] ;
+Bicicleta.add = function(aBici) {
+    Bicicleta.allBicis.push(aBici) ;
+}
+
+Bicicleta.findById = function(aBiciId) {
+  //var aBici = Bicicleta.allBicis.find(x => x.if == aBiciId)  // error
+  //var aBici = Bicicleta.allBicis.find(x => x.id.if == aBiciId)  //error
+    var aBici = Bicicleta.allBicis.find(x => x.if == Bicicleta.allBicis[aBiciId])
+    console.log(aBici)
+    if (aBici)
+        return aBici ;
+    else  
+        throw new Error(`No existe una bicleta con el id ${aBiciId}`) ;
+}
+
+Bicicleta.removeById = function(aBiciId) {
+   var aBici = Bicicleta.findById(aBiciId) ; //no es necesario porque abajo se busca uno a uno.
+    for(var i=0; i < Bicicleta.allBicis.length ; i++){
+        if (Bicicleta.allBicis[i].id == aBiciId) {
+            Bicicleta.allBicis.splice(i,1) ;
+            break ;
+        }
+    }
+}
+
+
+var a = new Bicicleta(1, 'rojo' , 'urbana' ,[10.9685400,-74.7813200]) ;
+var b = new Bicicleta(2, 'blanca' , 'urbana' ,[10.9685450,-74.7813250]) ;
+
+Bicicleta.add(a) ;
+Bicicleta.add(b) ;
+
+module.exports = Bicicleta ;
