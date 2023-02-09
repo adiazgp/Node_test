2c2a4d050cce3ca81b0983014c45c63e43bfdecf
diazag <adiazgp@gmail.com>
2023-02-09 16:33:20 -0500

    Commit_04

--- controllers/api/bicicletaControllerAPI.js
+++ controllers/api/bicicletaControllerAPI.js
@@ -16,3 +16,12 @@ exports.bicicleta_delete = function(req,res){
     Bicicleta.removeById(req.body.id) ;
     res.status(204).send() ;
 }
+
+exports.bicicleta_update = function(req,res){
+    var bici = Bicicleta.findById(req.body.id) ;
+    bici.id = req.body.id ;
+    bici.color = req.body.color ;
+    bici.modelo = req.body.modelo ;
+    bici.ubicacion = [req.body.latitud,req.body.longitud] ;
+    res.status(200).json({bicicleta: bici}) ;
+}
